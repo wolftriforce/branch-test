3afc8b006b9f02aa8c77f114c77b1be0434ea386
Rooseveltdelagarza <110432712+Rooseveltdelagarza@users.noreply.github.com>
2022-09-03 11:40:56 -0500

    imgagen

--- src/App.js
+++ src/App.js
@@ -9,16 +9,18 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <div>
+            <img src="" alt="" />
+          </div>
+          <div>
+            <img src="" alt="" />
+          </div>
+        </div>
       </header>
     </div>
+
+    
   );
 }
 
